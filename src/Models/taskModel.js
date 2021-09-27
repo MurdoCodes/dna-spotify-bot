860d6babf84562b3db729c191f89b4eae82a4e13
Lidel Kim Daddie <murdoc21daddie@gmail.com>
2021-09-27 22:21:02 +0800

    Update on fetch all users returning spotify user account details

--- src/Models/taskModel.js
+++ src/Models/taskModel.js
@@ -5,7 +5,8 @@ module.exports = class Task{
     
     static fetchAllTask(user_id){
         return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM ds_spotify_bot.spotify_task INNER JOIN ds_spotify_bot.spotify_users ON ds_spotify_bot.spotify_users.users_idusers = ds_spotify_bot.spotify_task.users_idusers WHERE ds_spotify_bot.spotify_task.users_idusers = ?'
+            const query = 'SELECT * FROM ds_spotify_bot.spotify_task INNER JOIN ds_spotify_bot.spotify_users ON ds_spotify_bot.spotify_users.spotify_user_id = ds_spotify_bot.spotify_task.potify_users_id WHERE ds_spotify_bot.spotify_task.users_idusers = ?'
+
             db.query(query, [user_id], (err, result) => {
                 if(err){
                     return reject(err)
